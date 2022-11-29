83aa08fc65f081af90fc202d6766abeb88f2ad61
omolola taiwo <omololataiwo93@gmail.com>
2022-11-28 18:07:01 -0800

    concat edge

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin//python3
+str = "python is an interpreted,interractive,object-oriented programming\
+       language tat comibine  remarkable power with every clear syntax"
+       str = str[39:67] + str[107:112] + str[:6]
+       print(str)

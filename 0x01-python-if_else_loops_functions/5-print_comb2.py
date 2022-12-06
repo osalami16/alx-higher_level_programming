f001c221a140d8cd4828d05fe8fbdccb771e9fae
omolola taiwo <omololataiwo93@gmail.com>
2022-12-06 08:27:09 -0800

    if else looping

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3 
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-         else:
-             print("{:02}".format(number), end=", ")
+#!/usr/bin/python3
+for i in range(100):
+    if i < 99:
+        print("{:02}".format(i), end=", ")
+    else:
+        print("{:02}".format(i))

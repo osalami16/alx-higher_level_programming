c5f0f54c1df0b1a531b5a854fda5e34d048841e8
omolola taiwo <omololataiwo93@gmail.com>
2023-04-03 14:25:16 -0700

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-Script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8).
-"""
-import urllib.request
-import urllib.parse
 import sys
+import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    param = {
-        "email": email
-    }
-    query_string = urllib.parse.urlencode(param)
-    data = query_string.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        response_text = response.read().decode('utf-8')
-        print(response_text)
 
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
